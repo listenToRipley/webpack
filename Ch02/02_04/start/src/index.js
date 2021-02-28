1e7135223fd1fe25ce069af11cb4635fc441e746
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-02-27 20:22:18 -0600

    update to react from jquery

--- Ch02/02_04/start/src/index.js
+++ Ch02/02_04/start/src/index.js
@@ -1,3 +1,9 @@
-const $ = require("jquery");
+import React from 'react';
+import {render} from 'react-dom';
 
-$("#target").html("hello everybody!!!!!!");
+const Greeting = () => <h1>Hello from React Webpack</h1>
+
+render(
+  <Greeting/>,
+  document.getElementById('target')
+)
