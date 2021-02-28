2fd9cc04425ef7a04a2fe4d4116bfa2f16cdf1a4
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-02-27 23:29:29 -0600

    update css

--- Ch03/03_02/start/src/index.js
+++ Ch03/03_02/start/src/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { render } from 'react-dom'
 import './style.css'
 
-const Greeting = () => <h1>Hello from React</h1>
+const Greeting = () => {
+    <div>
+        <h1>Webpack loading urls</h1>
+        <div id="image"></div>
+    </div>
+}
 
 render(
     <Greeting />,
     document.getElementById('target')
-)
+) 
