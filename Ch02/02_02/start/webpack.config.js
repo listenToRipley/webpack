52c527700eb1fb92055871e6e390079064dfe696
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-02-27 19:27:23 -0600

    update to webpack

--- Ch02/02_02/start/webpack.config.js
+++ Ch02/02_02/start/webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, //when a javascript is found, it should get transformed
+                exclude: /(node_modules)/, //don't do this with node modules, that will take forever
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+
+        ]
     }
 }
